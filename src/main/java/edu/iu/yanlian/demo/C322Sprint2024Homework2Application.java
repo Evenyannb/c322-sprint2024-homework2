5a1c4cfb70be4dc18e40e226326f62dd6a673aa0
yanlian <yanlian@iu.edu>
2024-01-29 17:29:20 -0500

    Initial commit

---
+++ src/main/java/edu/iu/yanlian/demo/C322Sprint2024Homework2Application.java
@@ -0,0 +1,13 @@
+package edu.iu.yanlian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C322Sprint2024Homework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C322Sprint2024Homework2Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/yanlian/demo/C322Sprint2024Homework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.yanlian.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C322Sprint2024Homework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
